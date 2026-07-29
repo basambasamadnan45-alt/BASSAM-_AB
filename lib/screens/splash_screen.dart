8ba387886d9d520feb6cd3c0f4bf7fa1aba9ae82
basambasamadnan45-alt <basambasamadnan45@gmail.com>
2026-07-30 02:25:49 +0300

    Update splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,1 +1,62 @@
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
 
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (_) => const LoginScreen(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Icon(
+              Icons.people_alt,
+              size: 90,
+              color: Colors.white,
+            ),
+            SizedBox(height: 20),
+            Text(
+              'BASSAM AB',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'أفضل منصة تواصل اجتماعي',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
