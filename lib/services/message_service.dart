c35598014fba3efd431107e35cb49fa641d736ca
basambasamadnan45-alt <basambasamadnan45@gmail.com>
2026-07-30 02:19:08 +0300

    Update message_service.dart

--- lib/services/message_service.dart
+++ lib/services/message_service.dart
@@ -1,1 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/message_model.dart';
 
+class MessageService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<void> sendMessage(MessageModel message) async {
+    await _firestore
+        .collection('messages')
+        .doc(message.id)
+        .set(message.toMap());
+  }
+
+  Stream<List<MessageModel>> getMessages(
+    String userId,
+    String otherUserId,
+  ) {
+    return _firestore
+        .collection('messages')
+        .where('senderId', whereIn: [userId, otherUserId])
+        .snapshots()
+        .map((snapshot) {
+      return snapshot.docs
+          .map((doc) => MessageModel.fromMap(doc.data()))
+          .toList();
+    });
+  }
+
+  Future<void> markAsSeen(String messageId) async {
+    await _firestore
+        .collection('messages')
+        .doc(messageId)
+        .update({
+      'isSeen': true,
+    });
+  }
+
+  Future<void> deleteMessage(String messageId) async {
+    await _firestore
+        .collection('messages')
+        .doc(messageId)
+        .delete();
+  }
+}
