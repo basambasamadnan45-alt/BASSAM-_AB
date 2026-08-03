4a2dab3a9289d995200efb7fc9030b7322df748a
basambasamadnan45-alt <basambasamadnan45@gmail.com>
2026-08-03 12:26:52 +0000

    Initialize Firebase in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/login_screen.dart';
@@ -8,7 +10,13 @@ import 'screens/chat_screen.dart';
 import 'screens/settings_screen.dart';
 
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const ConnectAB());
 }
 
