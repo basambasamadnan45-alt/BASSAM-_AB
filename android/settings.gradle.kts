d1a70e168768beb5223454ad3443752a0aefafe0
basambasamadnan45-alt <basambasamadnan45@gmail.com>
2026-08-05 23:49:31 +0300

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -19,11 +19,19 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.7.3" apply false
-    // START: FlutterFire Configuration
-    id("com.google.gms.google-services") version("4.3.15") apply false
-    // END: FlutterFire Configuration
-    id("org.jetbrains.kotlin.android") version "2.1.21" apply false
+    id("com.android.application") version "8.11.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    id("com.google.gms.google-services") version "4.4.3" apply false
 }
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "connect_ab"
 
 include(":app")
